400a2827a1f17d04f99ad29ed5282a28b917b276
Lim Cheng Yang <chengyang1017@gmail.com>
2026-09-02 15:48:25 +0800

    Refactor NScrollColumn to extension-first composition

--- lib/src/layouts/n_scroll_column.dart
+++ lib/src/layouts/n_scroll_column.dart
@@ -46,22 +46,23 @@ class NScrollColumn extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return NColumn(
-      constraints: constraints,
-      margin: margin,
-      alignment: alignment,
-      decoration: decoration,
-      gap: gap,
-      mainAxisAlignment: mainAxisAlignment,
-      mainAxisSize: mainAxisSize,
-      crossAxisAlignment: crossAxisAlignment,
-      children: children,
-    )
+    return children
+        .nColumn(
+          gap: gap,
+          constraints: constraints,
+          margin: margin,
+          alignment: alignment,
+          decoration: decoration,
+          mainAxisAlignment: mainAxisAlignment,
+          mainAxisSize: mainAxisSize,
+          crossAxisAlignment: crossAxisAlignment,
+        )
         .nScrollY(
           controller: controller,
           physics: physics,
           padding: padding,
           reverse: reverse,
+          clipBehavior: clipBehavior,
         )
         .nSize(
           width: width,
@@ -69,72 +70,3 @@ class NScrollColumn extends StatelessWidget {
         );
   }
 }
-
-// import 'package:flutter/material.dart';
-
-// import 'n_column.dart';
-
-// class NScrollColumn extends StatelessWidget {
-//   final List<Widget> children;
-//   final double gap;
-//   final double? width;
-//   final double? height;
-//   final BoxConstraints? constraints;
-//   final EdgeInsetsGeometry padding;
-//   final EdgeInsetsGeometry? margin;
-//   final AlignmentGeometry? alignment;
-//   final Decoration? decoration;
-//   final MainAxisAlignment mainAxisAlignment;
-//   final MainAxisSize mainAxisSize;
-//   final CrossAxisAlignment crossAxisAlignment;
-//   final ScrollController? controller;
-//   final ScrollPhysics? physics;
-//   final bool reverse;
-//   final Clip clipBehavior;
-
-//   const NScrollColumn({
-//     super.key,
-//     required this.children,
-//     this.gap = 0,
-//     this.width,
-//     this.height,
-//     this.constraints,
-//     this.padding = EdgeInsets.zero,
-//     this.margin,
-//     this.alignment,
-//     this.decoration,
-//     this.mainAxisAlignment = MainAxisAlignment.start,
-//     this.mainAxisSize = MainAxisSize.min,
-//     this.crossAxisAlignment = CrossAxisAlignment.stretch,
-//     this.controller,
-//     this.physics,
-//     this.reverse = false,
-//     this.clipBehavior = Clip.hardEdge,
-//   });
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return SizedBox(
-//       width: width,
-//       height: height,
-//       child: SingleChildScrollView(
-//         controller: controller,
-//         physics: physics,
-//         reverse: reverse,
-//         padding: padding,
-//         clipBehavior: clipBehavior,
-//         child: NColumn(
-//           constraints: constraints,
-//           margin: margin,
-//           alignment: alignment,
-//           decoration: decoration,
-//           gap: gap,
-//           mainAxisAlignment: mainAxisAlignment,
-//           mainAxisSize: mainAxisSize,
-//           crossAxisAlignment: crossAxisAlignment,
-//           children: children,
-//         ),
-//       ),
-//     );
-//   }
-// }
