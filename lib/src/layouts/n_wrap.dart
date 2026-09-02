a89dcde4f63b822c8494e2a14d8efff657a584fd
Lim Cheng Yang <chengyang1017@gmail.com>
2026-09-02 15:50:31 +0800

    Add extension-first wrap API

--- lib/src/layouts/n_wrap.dart
+++ lib/src/layouts/n_wrap.dart
@@ -68,70 +68,43 @@ class NWrap extends StatelessWidget {
   }
 }
 
-// import 'package:flutter/material.dart';
-
-// import 'n_box.dart';
-
-// class NWrap extends StatelessWidget {
-//   final List<Widget> children;
-//   final double spacing;
-//   final double runSpacing;
-//   final double? width;
-//   final double? height;
-//   final BoxConstraints? constraints;
-//   final EdgeInsetsGeometry? padding;
-//   final EdgeInsetsGeometry? margin;
-//   final Decoration? decoration;
-//   final Axis direction;
-//   final WrapAlignment alignment;
-//   final WrapAlignment runAlignment;
-//   final WrapCrossAlignment crossAxisAlignment;
-//   final TextDirection? textDirection;
-//   final VerticalDirection verticalDirection;
-//   final Clip clipBehavior;
-
-//   const NWrap({
-//     super.key,
-//     required this.children,
-//     this.spacing = 0,
-//     this.runSpacing = 0,
-//     this.width,
-//     this.height,
-//     this.constraints,
-//     this.padding,
-//     this.margin,
-//     this.decoration,
-//     this.direction = Axis.horizontal,
-//     this.alignment = WrapAlignment.start,
-//     this.runAlignment = WrapAlignment.start,
-//     this.crossAxisAlignment = WrapCrossAlignment.start,
-//     this.textDirection,
-//     this.verticalDirection = VerticalDirection.down,
-//     this.clipBehavior = Clip.none,
-//   });
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return NBox(
-//       width: width,
-//       height: height,
-//       constraints: constraints,
-//       padding: padding,
-//       margin: margin,
-//       decoration: decoration,
-//       clipBehavior: clipBehavior,
-//       child: Wrap(
-//         direction: direction,
-//         alignment: alignment,
-//         runAlignment: runAlignment,
-//         spacing: spacing,
-//         runSpacing: runSpacing,
-//         crossAxisAlignment: crossAxisAlignment,
-//         textDirection: textDirection,
-//         verticalDirection: verticalDirection,
-//         clipBehavior: clipBehavior,
-//         children: children,
-//       ),
-//     );
-//   }
-// }
+extension NestlessWrapExtensions on Iterable<Widget> {
+  Widget nWrap({
+    double spacing = 0,
+    double runSpacing = 0,
+    double? width,
+    double? height,
+    BoxConstraints? constraints,
+    EdgeInsetsGeometry? padding,
+    EdgeInsetsGeometry? margin,
+    Decoration? decoration,
+    Axis direction = Axis.horizontal,
+    WrapAlignment alignment = WrapAlignment.start,
+    WrapAlignment runAlignment = WrapAlignment.start,
+    WrapCrossAlignment crossAxisAlignment = WrapCrossAlignment.start,
+    TextDirection? textDirection,
+    VerticalDirection verticalDirection = VerticalDirection.down,
+    Clip clipBehavior = Clip.none,
+  }) {
+    return Wrap(
+      direction: direction,
+      alignment: alignment,
+      runAlignment: runAlignment,
+      spacing: spacing,
+      runSpacing: runSpacing,
+      crossAxisAlignment: crossAxisAlignment,
+      textDirection: textDirection,
+      verticalDirection: verticalDirection,
+      clipBehavior: clipBehavior,
+      children: toList(growable: false),
+    ).nBox(
+      width: width,
+      height: height,
+      constraints: constraints,
+      padding: padding,
+      margin: margin,
+      decoration: decoration,
+      clipBehavior: clipBehavior,
+    );
+  }
+}
